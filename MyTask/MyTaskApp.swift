f0dac44b3ece36eff042c39174e561111f03b30b
afzal_ahmed <ahmed.iosdev101@gmail.com>
2025-02-22 01:51:14 +0500

    Initial Commit

---
+++ MyTask/MyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTaskApp.swift
+//  MyTask
+//
+//  Created by Apple on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
